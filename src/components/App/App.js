f6b4a2b7ed3b6c1001e1eccef6a3d2f365a2406b
Alex Lyubakov <lyubakovaa@gmail.com>
2018-12-19 18:46:18 +0400

    empty

---
+++ src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class newComponent extends Component{
+    state = {smth: 22};
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
